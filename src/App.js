e4184d3aaf87146f1a6c5b1905e29e05bd1d0b88
Elizabeth Zachary <elizabethzachary96@gmail.com>
2023-06-21 20:05:56 -0700

    implemented useEffect to retrieve all tasks from backend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import TaskList from './components/TaskList.js';
 import './App.css';
+import axios from 'axios';
+
+
+const BASE_URL = 'http://localhost:5000/tasks';
 
 // starter data to assign as default data in useState
-const TASKS = [
-  {
-    id: 1,
-    title: 'Mow the lawn',
-    isComplete: false,
-  },
-  {
-    id: 2,
-    title: 'Cook Pasta',
-    isComplete: false,
-  },
-];
+// const TASKS = [
+//   {
+//     id: 1,
+//     title: 'Mow the lawn',
+//     isComplete: false,
+//   },
+//   {
+//     id: 2,
+//     title: 'Cook Pasta',
+//     isComplete: false,
+//   },
+// ];
 
 const App = () => {
-  const [ taskData, setTaskData ] = useState(TASKS);
+  const [ taskData, setTaskData ] = useState([]);
 
   // function to take the updated task from the Task component
   // and update the TaskData to reflect that changes to the updated task
@@ -33,6 +37,14 @@ const App = () => {
     setTaskData(updatedTasks);
   };
 
+  // set useEffect to get all tasks from our backend
+  useEffect(() => {
+    axios.get(BASE_URL)
+    .then((res) => setTaskData(res.data))
+    .catch((err) => console.log(err));
+  }, []);
+
+  console.log(taskData);
   // function to filter out the task with the matching id
   // from our previous task data
   const deleteTask = (id) => {
